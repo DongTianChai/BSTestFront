1b9872b4e0ef6057462dd1e5627839be5d81605f
东天柴“ <3396856578@qq.com>
2024-10-17 08:30:19 +0800

    初始

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,29 @@
 //初始化一个router数组，数组中添加路由对象，每一个对象都有一个path属性和一个component属性，path属性表示路由的路径，component属性表示路由对应的组件
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import Login from '@/components/login/Login'
+
+
+import index from '@/components/index/Index'
+
+import permList from "@/components/user/PermList.vue";
 Vue.use(Router)
 
 export default new Router({
   routes: [
+
     {
       path: '/',
-      name: 'Login',
-      component: Login
+      name: 'Index',
+      component: index,
+
+      redirect: '/permList', // 设置默认路由
+      children:[
+        //子路径的path路径不能加“/”
+
+
+        {path:'permList',name:'PermList',component:permList}
+      ]
     },
-    {
-      path: '/hello',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+
   ]
 })
